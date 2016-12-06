cbf9083afe9d3f647a40f042d8b892f2fce421b7
derex pan <derexpan@derexdembp.lan>
2016-12-06 23:08:36 +0800

    Initial Commit

---
+++ jiagexian_Swfit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jiagexian_Swfit
+//
+//  Created by derex pan on 2016/12/6.
+//  Copyright © 2016年 derex pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
