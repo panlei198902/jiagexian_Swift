66707bf66f53eca730814373ff277946f2e1211e
panlei198902 <panlei198902@gmail.com>
2016-12-09 21:24:08 +0800

    updated the networking framework

---
+++ HotelBL.swift
@@ -0,0 +1,13 @@
+//
+//  HotelBL.swift
+//  jiagexian_Swift
+//
+//  Created by derex pan on 2016/12/9.
+//  Copyright © 2016年 derex pan. All rights reserved.
+//
+
+import UIKit
+
+class HotelBL: NSObject {
+
+}
---
+++ jiagexian_Swfit/ss.m
@@ -0,0 +1,9 @@
+//
+//  ss.m
+//  jiagexian_Swift
+//
+//  Created by derex pan on 2016/12/8.
+//  Copyright © 2016年 derex pan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
