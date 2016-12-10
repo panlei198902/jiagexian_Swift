f93cd56018c38be26fcff37b5879935e298b7c48
panlei198902 <panlei198902@gmail.com>
2016-12-10 23:28:01 +0800

    引入XML解析第三方框架

--- jiagexian_Swfit/ViewController.swift
+++ jiagexian_Swfit/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let hotelBL = HotelBL.sharedInstance
+//        let returnss = hotelBL.selectKey(city: "北京")
+//        let returns11 = hotelBL.queryHotel(keyInfo: <#T##NSDictionary#>)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
